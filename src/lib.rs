e2cb2d8cdbeada1fac9968f0f5ba304b35135425
Pierre Barre <pierre@barre.sh>
2024-09-07 02:03:24 +0200

    Implement cli

---
+++ src/lib.rs
@@ -0,0 +1,85 @@
+use anyhow::Result;
+use chrono::{DateTime, TimeZone, Utc};
+use futures::StreamExt;
+use reqwest_eventsource::{Error as EventSourceError, Event, EventSource};
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct Entry {
+    domain: String,
+    subject_common_name: Option<String>,
+    not_before: i64,
+}
+
+#[derive(Debug, Deserialize)]
+struct TailEntry {
+    hostname: String,
+}
+
+trait Printable {
+    fn print(&self);
+}
+
+impl Printable for Entry {
+    fn print(&self) {
+        let timestamp = Utc.timestamp_opt(self.not_before, 0).unwrap();
+        println!(
+            "domain={} subject_common_name={} not_before={} human_readable_not_before={}",
+            self.domain,
+            if let Some(subject_common_name) = &self.subject_common_name {
+                subject_common_name
+            } else {
+                "N/A"
+            },
+            self.not_before,
+            format_timestamp(timestamp)
+        );
+    }
+}
+
+impl Printable for TailEntry {
+    fn print(&self) {
+        let current_time: DateTime<Utc> = Utc::now();
+        println!(
+            "hostname={} timestamp={} human_readable_not_before={}",
+            self.hostname,
+            current_time.to_rfc3339(),
+            format_timestamp(current_time)
+        );
+    }
+}
+
+async fn process_event_stream<T>(url: &str) -> Result<()>
+where
+    T: for<'de> Deserialize<'de> + Printable,
+{
+    let mut es = EventSource::get(url);
+
+    while let Some(event) = es.next().await {
+        match event {
+            Ok(Event::Message(message)) => {
+                let entry = serde_json::from_str::<T>(&message.data)?;
+                entry.print();
+            }
+            Ok(Event::Open) => {}
+            Err(EventSourceError::StreamEnded) => break,
+            Err(e) => eprintln!("Error: {:?}", e),
+        }
+    }
+
+    Ok(())
+}
+
+fn format_timestamp(timestamp: DateTime<Utc>) -> String {
+    timestamp.format("%Y-%m-%d %H:%M:%S UTC").to_string()
+}
+
+pub async fn search(query: &str) -> Result<()> {
+    let url = format!("https://api.merklemap.com/search?query={}&stream=true", query);
+    process_event_stream::<Entry>(&url).await
+}
+
+pub async fn tail() -> Result<()> {
+    let url = "https://api.merklemap.com/live-domains?no_throttle=true";
+    process_event_stream::<TailEntry>(url).await
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,33 @@
+use clap::{Parser, Subcommand};
+use merklemap_cli::{search, tail};
+
+#[derive(Parser)]
+#[command(author, version, about = "A merklemap.com api client", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Search for subdomains matching a given query.
+    Search { query: String },
+    /// Tail live subdomains discoveries from the Merklemap ingestion pipeline.
+    Tail,
+}
+
 #[tokio::main]
-async fn main() {
-    println!("Hello, world!");
+async fn main() -> anyhow::Result<()> {
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Search { query } => {
+            search(query).await?;
+        }
+        Commands::Tail => {
+            tail().await?;
+        }
+    }
+
+    Ok(())
 }
